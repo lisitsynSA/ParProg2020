7487290ceab7d6a8d5dd5e8bb6750947a10ccd8c
redf1sh <unreal.770@gmail.com>
2020-10-03 21:35:46 +0300

    Added solution for second openMP task

--- OpenMP2/src/main.cpp
+++ OpenMP2/src/main.cpp
@@ -3,9 +3,67 @@
 #include <fstream>
 #include <omp.h>
 
+double calc3(uint32_t x_last, uint32_t num_threads)
+{
+    omp_set_dynamic( 0);
+    omp_set_num_threads( num_threads);
+
+    double res = 0.0;
+    uint32_t i;
+             
+    double* res_buffer = (double*)calloc(x_last, sizeof(double));
+
+    #pragma omp parallel for num_threads( num_threads) 
+    for( i = x_last; i > 0; i--)
+    {
+        res_buffer[ i - 1] = 1.0 / i;
+    }
+
+    for ( i = x_last; i > 0; i--)
+        res += res_buffer[ i - 1];
+    
+    return res;
+}
+
+
+double calc2(uint32_t x_last, uint32_t num_threads)
+{
+    omp_set_dynamic( 0);
+    omp_set_num_threads( num_threads);
+
+    double res = 0.0;
+    uint32_t i;
+             
+    #pragma omp parallel for reduction(+:res) num_threads( num_threads) 
+    for( i = x_last; i > 0; i--)
+    {
+        res += 1.0 / i;
+    }
+    
+    return res;
+}
+
+       
 double calc(uint32_t x_last, uint32_t num_threads)
 {
-  return 0;
+    omp_set_dynamic( 0);
+    omp_set_num_threads( num_threads);
+
+    double res = 0.0;
+    uint32_t i;
+    
+    double* res_buffer = (double*)calloc(num_threads, sizeof(double));
+        
+    #pragma omp parallel for num_threads( num_threads) 
+    for( i = x_last; i > 0; i--)
+    {
+        res_buffer[ omp_get_thread_num()] += 1.0 / i;
+    }
+    
+    for (int i = num_threads; i >= 0; i--)
+        res += res_buffer[ i];
+    
+    return res;
 }
 
 int main(int argc, char** argv)
@@ -38,7 +96,7 @@ int main(int argc, char** argv)
   uint32_t x_last = 0, num_threads = 0;
   input >> x_last >> num_threads;
   // Calculation
-  double res = calc(x_last, num_threads);
+  double res = calc3(x_last, num_threads);
 
   // Write result
   output << std::setprecision(15) << res << std::endl;
