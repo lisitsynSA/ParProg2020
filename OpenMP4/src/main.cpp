123eb8dc818846b4a478a10a72fb7d9752c1be5e
redf1sh <unreal.770@gmail.com>
2020-10-16 22:45:04 +0300

    Added solution for OpenMP4 and game 'Life'

--- OpenMP4/src/main.cpp
+++ OpenMP4/src/main.cpp
@@ -2,10 +2,78 @@
 #include <iomanip>
 #include <fstream>
 #include <omp.h>
+#include <gmpxx.h>
 
+
+void fact(int n){
+  int i;
+  mpz_t p;
+
+  mpz_init_set_ui(p,1); /* p = 1 */
+
+  for (i=1; i <= n ; ++i){
+    mpz_mul_ui(p,p,i); /* p = p * i */
+  }
+  printf ("%d!  =  ", n);
+
+}
+
+#if 0
+/**
+ * Осуществляет принцип "разделяй и властвуй", где превращает список множителей в бинарное дерево
+ * и перемножает их по 2.
+ *
+ * Рекурсивный алгоритм.
+ */
+double pod_tree_rec( int l, int r)
+{
+    if( l > r)
+        return 1;
+
+    if( l == r)
+        return l;
+
+    if( ( r - l) == 1)
+        return (double)(l * r);
+
+    int m = (l + r) / 2;
+    
+    return (pod_tree_rec( l, m) * pod_tree_rec( m + 1, r));
+}
+
+double tree_factorial( int n)
+{
+    if ( ( n == 0))
+        return 1;
+
+    if ( ( n == 2) || ( n == 1))
+        return n;
+
+    return ( pod_tree_rec( 2, n));
+}
+
+#endif
 double calc(uint32_t x_last, uint32_t num_threads)
 {
-  return 0;
+    omp_set_dynamic( 0);
+    omp_set_num_threads( num_threads);
+    
+    //double res = 1.0;
+    mpz_t res;
+    double *factorial_array = (double*)calloc( x_last + 1, sizeof(double));
+    factorial_array[0] = 1;
+
+    #pragma omp parallel for num_threads( num_threads)
+    for( uint32_t i = 1; i < x_last; i++)
+        factorial_array[i] = tree_factorial( i);
+
+    #pragma omp parallel for reduction(+:res) num_threads( num_threads)
+    for( uint32_t i = 1; i < x_last - 1; i++)
+    {
+        res += 1.0 / factorial_array[i];
+    }
+
+    return res;
 }
 
 int main(int argc, char** argv)
--- OpenMP5/src/main.cpp
+++ OpenMP5/src/main.cpp
@@ -3,9 +3,153 @@
 #include <fstream>
 #include <omp.h>
 
-void calc(uint32_t xSize, uint32_t ySize, uint32_t iterations, uint32_t num_threads, uint8_t* inputFrame, uint8_t* outputFrame)
-{
+#define DEAD '0'
+#define ALIVE '1'
 
+/**
+ * Конвертирует массив в 2D для более удобной работы
+ *
+ * WARNING Массив удаляется из вызывающей функции
+ */
+uint8_t** ConvertFrom1Dto2D( uint32_t xSize, uint32_t ySize, uint8_t* input_frame)
+{
+    uint8_t** temp_frame = (uint8_t**)calloc( ySize, sizeof(uint8_t*));
+    for ( uint32_t i = 0; i < xSize; i++)
+    {
+        temp_frame[i] = (uint8_t*)calloc( ySize, sizeof(uint8_t));
+    }
+    
+    /* x - столбцы, y - строки */
+    for ( uint32_t i = 0; i < ySize; i++)
+        for ( uint32_t j = 0; j < xSize; j++)
+            temp_frame[i][j] = input_frame[i * ySize + j];
+    return ( temp_frame);
+}
+
+/**
+ * Переводит матрицу в линейный массив
+ */
+uint8_t* ConvertFrom2Dto1D( uint32_t xSize, uint32_t ySize, uint8_t** input_frame)
+{
+    
+    uint8_t* temp_frame = (uint8_t*)calloc( ySize * xSize, sizeof(uint8_t));
+
+    for ( uint32_t i = 0; i < ySize; i++)
+	for( uint32_t j = 0; j < xSize; j++)
+	    temp_frame[i * xSize + j] = input_frame[i][j];
+
+    return ( temp_frame);
+}
+
+/**
+ * Вычисляет количество соседей возле указанной точки (x, y)
+ */
+int NumOfNeighbours( uint32_t xSize, uint32_t ySize, uint32_t x, uint32_t y, uint8_t** input_frame)
+{
+    int neighbours_num = 0;
+    xSize--;
+    ySize--;
+    /* Необходимо вокруг посмотреть 8 клеток, если там, кто-то есть, то количество соседей увеличиваем на 1 */
+
+    /* Левый верхний угол */
+    if ( ( x >= 1) && ( y >= 1))
+    	if ( input_frame[x - 1][y - 1] == ALIVE)
+	    neighbours_num++;
+    
+    /* Верхний средний */
+    if ( x >= 1)
+	if ( input_frame[x - 1][y] == ALIVE)
+	    neighbours_num++;
+
+    /* Правый сверху */
+    if ( ( x >= 1) && ( y <= ( ySize - 1)))
+	if ( input_frame[x - 1][y + 1] == ALIVE)
+	     neighbours_num++;
+
+    /* Слева средний */
+    if ( y >= 1)
+	if ( input_frame[x][y - 1] == ALIVE)
+	    neighbours_num++;
+
+    /* Справа средний */
+    if ( y <= ( ySize - 1))
+	if ( input_frame[x][y + 1] == ALIVE)
+	    neighbours_num++;
+
+    /* Слева нижний */
+    if ( ( x <= ( xSize - 1)) && ( y >= 1))
+	if ( input_frame[x + 1][y - 1] == ALIVE) 
+	    neighbours_num++;
+
+    /* Нижний средний */
+    if ( x <= ( xSize - 1))
+	if ( input_frame[x + 1][y] == ALIVE)
+	    neighbours_num++;
+
+    /* Справа нижний */      
+    if ( ( y <= ( ySize - 1)) && ( x <= (xSize - 1)))
+	if ( input_frame[x + 1][y + 1] == ALIVE)
+	    neighbours_num++;
+
+    return ( neighbours_num);
+}
+
+
+uint8_t* calc(uint32_t xSize, uint32_t ySize, uint32_t iterations, uint32_t num_threads, uint8_t* inputFrame)
+{
+    if (iterations == 0)
+    {
+	return inputFrame;
+    }
+
+
+    omp_set_dynamic( 0);
+    omp_set_num_threads( num_threads);
+
+    /* Для каждой ячейки необходимо проверить соседние и посчитать количество соседей */
+    uint8_t** temp_frame = ConvertFrom1Dto2D( xSize, ySize, inputFrame);
+   
+    uint8_t** result_frame = (uint8_t**)calloc( xSize, sizeof(uint8_t*));
+    for ( uint32_t i = 0; i < xSize; i++)
+    {
+        result_frame[i] = (uint8_t*)calloc( ySize, sizeof(uint8_t));
+    }
+    
+    //#pragma omp parallel for num_threads( num_threads) 
+    for ( uint32_t k = 0; k < iterations; k++)
+    {
+	for ( uint32_t i = 0; i < xSize; i++)
+	{
+	    for ( uint32_t j = 0; j < ySize; j++)
+	    {
+		/* В пустой клетке, рядом с которой есть ровно 3 соседа, зарождается жизнь */
+		if ( ( NumOfNeighbours( xSize, ySize, i, j, temp_frame) == 3) && (temp_frame[i][j] == DEAD))
+		    result_frame[i][j] = ALIVE;
+		else
+		/* Если у живой клетки 2 или 3 соседа, то она продолжает жить */
+		if ( ( ( NumOfNeighbours( xSize, ySize, i, j, temp_frame) == 2) || ( NumOfNeighbours( xSize, ySize, i, j, temp_frame) == 3)) && (temp_frame[i][j] == ALIVE))
+		    result_frame[i][j] = ALIVE;
+		else
+		    result_frame[i][j] = DEAD;
+	    }
+	}
+
+	temp_frame = result_frame;
+    }
+
+
+    return ( ConvertFrom2Dto1D( xSize, ySize, result_frame));
+    
+/*
+    for ( uint32_t i = 0; i < xSize; i++)
+    {
+	free( temp_frame[i]);
+	free( result_frame[i]);
+    }
+
+    free( temp_frame);
+    free( result_frame);
+    */
 }
 
 int main(int argc, char** argv)
@@ -47,8 +191,9 @@ int main(int argc, char** argv)
     }
   }
 
+
   // Calculation
-  calc(xSize, ySize, iterations, num_threads, inputFrame, outputFrame);
+ outputFrame = calc(xSize, ySize, iterations, num_threads, inputFrame);
 
   // Write result
   for (uint32_t y = 0; y < ySize; y++)
@@ -59,10 +204,11 @@ int main(int argc, char** argv)
     }
     output << "\n";
   }
-
+#if 0
   // Prepare to exit
   delete outputFrame;
   delete inputFrame;
+#endif  
   output.close();
   input.close();
   return 0;
