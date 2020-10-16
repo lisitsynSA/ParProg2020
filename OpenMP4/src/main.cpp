91fbdb2e777d7759f0e6ad8f57d5a2da1ded4756
ks <kostyamikhaylov>
2020-10-16 16:53:01 +0300

    Third and (probably) fourth tasks

--- OpenMP4/src/main.cpp
+++ OpenMP4/src/main.cpp
@@ -3,9 +3,44 @@
 #include <fstream>
 #include <omp.h>
 
-double calc(uint32_t x_last, uint32_t num_threads)
+
+double KahanSumReversed (double *arr, int len)
 {
-  return 0;
+	double sum = 0.0, error = 0.0, y = 0.0, t = 0.0;
+	for (int i = len - 1; i >= 0; i--)
+	{
+		y = arr[i] - error;
+		t = sum + y;
+		error = (t - sum) - y;
+		sum = t;
+	}
+	return sum;
+}
+
+double calc (uint32_t x_last, uint32_t num_threads)
+{
+	double result = 0.0;
+	double *res_arr = NULL, *fact_arr = 0;
+	res_arr = (double *) calloc (x_last, sizeof (*res_arr));
+	fact_arr = (double *) calloc (x_last, sizeof (*res_arr));
+	if (!res_arr || !fact_arr)
+	{
+		fprintf (stderr, "Calloc error\n");
+		exit (-1);
+	}
+
+    fact_arr[0] = 1;
+    for (unsigned int i = 1; i < x_last; i++)
+        fact_arr[i] = fact_arr[i - 1] * i;
+
+	#pragma omp parallel for num_threads(num_threads)
+		for (unsigned int i = 0; i < x_last; i++)
+			res_arr[i] = 1.0 / fact_arr[i];
+
+	result = KahanSumReversed (res_arr, x_last);
+	free (res_arr);
+	free (fact_arr);
+	return result;
 }
 
 int main(int argc, char** argv)
