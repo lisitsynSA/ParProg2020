27fd01b18ae1ae92c70503354969bed5fb075e65
Daniil Vorobyev <vorobyev.da@phystech.edu>
2020-10-22 10:53:12 +0300

    OpenMP5 task is done

--- OpenMP5/src/main.cpp
+++ OpenMP5/src/main.cpp
@@ -5,7 +5,91 @@
 
 void calc(uint32_t xSize, uint32_t ySize, uint32_t iterations, uint32_t num_threads, uint8_t* inputFrame, uint8_t* outputFrame)
 {
+  int* inFrame = (int*)malloc(xSize*ySize * sizeof(int));
+  int* outFrame = (int *)malloc(xSize*ySize * sizeof(int));
 
+  for (uint32_t y = 0; y < ySize; y++) {
+    for (uint32_t x = 0; x < xSize; x++) {
+      outFrame[y * xSize + x] = (int)inputFrame[y * xSize + x];
+    }
+  }
+  uint32_t block_size = xSize / num_threads;
+  uint32_t block_extra = xSize % num_threads;
+  #pragma omp parallel num_threads(num_threads)
+  {
+    uint32_t tid = omp_get_thread_num();
+
+    uint32_t first = tid * block_size;
+    uint32_t last = (tid + 1) * block_size;
+
+    if (tid == num_threads - 1) {
+      last += block_extra;
+    }
+
+
+    for (uint32_t i = 0; i < iterations; i++) {
+      #pragma omp barrier
+      #pragma omp single 
+      {
+        int* ptr = inFrame;
+        inFrame = outFrame;
+        outFrame = ptr;
+      }
+      for (uint32_t x = first; x < last; x++) {
+          for (uint32_t y = 0; y < ySize; y++) {
+            uint32_t count = 0;
+
+            uint32_t x1 = x == 0 ? xSize - 1 : x - 1;
+            uint32_t x2 = x == xSize - 1 ? 0 : x + 1;
+            uint32_t y1 = y == 0 ? ySize - 1 : y - 1;
+            uint32_t y2 = y == ySize - 1 ? 0 : y + 1;
+
+
+            if (inFrame[y1 * xSize + x] == 1){
+              count++;
+            }
+            if (inFrame[y1 * xSize + x1] == 1) {
+              count++;
+            }
+            if (inFrame[y1 * xSize + x2] == 1) {
+              count++;
+            }
+            if (inFrame[y2 * xSize + x1] == 1) {
+              count++;
+            }
+            if (inFrame[y2 * xSize + x2] == 1) {
+              count++;
+            }
+
+            if (inFrame[y2 * xSize + x] == 1) {
+              count++;
+            }
+            if (inFrame[y * xSize + x1] == 1) {
+              count++;
+            }
+            if (inFrame[y * xSize + x2] == 1) {
+              count++;
+            }
+
+            if (inFrame[y * xSize + x] == 0 && count == 3) {
+              outFrame[y * xSize + x] = 1;
+            } else if (inFrame[y * xSize + x] == 1 && (count < 2 || count > 3)) {
+              outFrame[y * xSize + x] = 0;
+            } else { 
+              outFrame[y * xSize + x] = inFrame[y * xSize + x];
+            }
+        }
+      }
+    }
+  }
+
+  for (uint32_t y = 0; y < ySize; y++) {
+    for (uint32_t x = 0; x < xSize; x++) {
+      outputFrame[y * xSize + x] = outFrame[y * xSize + x];
+    }
+  }
+  free(inFrame);
+  free(outFrame);
 }
 
 int main(int argc, char** argv)
@@ -63,8 +147,8 @@ int main(int argc, char** argv)
   }
 
   // Prepare to exit
-  delete outputFrame;
-  delete inputFrame;
+  delete[] outputFrame;
+  delete[] inputFrame;
   output.close();
   input.close();
   return 0;
