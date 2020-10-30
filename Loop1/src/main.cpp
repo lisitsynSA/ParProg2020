f715c72ffae35b148746516ef1078a8a9b14376a
Extra5enS <molotkov.md@phystech.edu>
2020-10-30 14:49:34 +0300

    static impl

--- Loop1/src/main.cpp
+++ Loop1/src/main.cpp
@@ -4,104 +4,80 @@
 #include <mpi.h>
 #include <unistd.h>
 #include <cmath>
+#include <algorithm>
+
+#include <string.h>
+#include <cstdlib>
 
 #define ROOT 0
 
+#define S_TAG 1
 
+#define coord(x, y, xSize) ((y)*(xSize) + x)
 
-#define MPI_recv_start_data(worker, have_task) { \
-    MPI_Recv(worker, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, NULL); \
-    MPI_Recv(have_task, 1, MPI_INT, *worker, 1, MPI_COMM_WORLD, NULL); \
-}
+// arr[y*xSize + x] = sin(0.00001*arr[y*xSize + x]);
+void calc(double* arr, uint32_t ySize, uint32_t xSize, int rank, int size) {
+    int32_t step = 0;
+    int32_t* range = NULL;
+    int32_t* displace = NULL;
+    
+    MPI_Bcast(&xSize, 1, MPI_UNSIGNED, ROOT, MPI_COMM_WORLD);
+    MPI_Bcast(&ySize, 1, MPI_UNSIGNED, ROOT, MPI_COMM_WORLD);
+    
+    if(rank == ROOT) {        
+        uint32_t y_step = ceil(ySize / (1.0 * size));
+        uint32_t prev_range = 0;
+        
+        range = (int32_t*)calloc(size, sizeof(int32_t));
+        displace = (int32_t*)calloc(size, sizeof(int32_t));
 
-#define MPI_send_start_data(rank, have_task) { \
-    MPI_Send(rank, 1, MPI_INT, ROOT, 1, MPI_COMM_WORLD); \
-    MPI_Send(have_task, 1, MPI_INT, ROOT, 1, MPI_COMM_WORLD); \
-}
-
-#define MPI_recv_res(worker, arr, xSize) { \
-    uint32_t ux, uy; \
-    MPI_Recv(&ux, 1, MPI_UNSIGNED, worker, 2, MPI_COMM_WORLD, NULL); \
-    MPI_Recv(&uy, 1, MPI_UNSIGNED, worker, 2, MPI_COMM_WORLD, NULL); \
-    MPI_Recv(&(arr[uy*xSize + ux]), 1, MPI_DOUBLE, worker, 2, MPI_COMM_WORLD, NULL); \
-}  
-
-#define MPI_send_res(x, y, arr) { \
-    MPI_Send(x, 1, MPI_UNSIGNED, ROOT, 2, MPI_COMM_WORLD); \
-    MPI_Send(y, 1, MPI_UNSIGNED, ROOT, 2, MPI_COMM_WORLD); \
-    MPI_Send(arr, 1, MPI_DOUBLE, ROOT, 2, MPI_COMM_WORLD); \
-}
-
-#define MPI_recv_data(x, y, arr) { \
-    MPI_Recv(x, 1, MPI_UNSIGNED, ROOT, 1, MPI_COMM_WORLD, NULL); \
-    MPI_Recv(y, 1, MPI_UNSIGNED, ROOT, 1, MPI_COMM_WORLD, NULL); \
-    if(*x == uint32_t(-1) && *y == uint32_t(-1)) { \
-        break; \
-    } \
-    MPI_Recv(arr, 1, MPI_DOUBLE, ROOT, 1, MPI_COMM_WORLD, NULL); \
-}
-
-#define MPI_send_data(worker, x, y, arr, xSize, need_to_send_arr) { \
-    MPI_Send(x, 1, MPI_UNSIGNED, worker, 1, MPI_COMM_WORLD); \
-    MPI_Send(y, 1, MPI_UNSIGNED, worker, 1, MPI_COMM_WORLD); \
-    if(need_to_send_arr) { \
-        MPI_Send(&arr[*y * xSize + *x], 1, MPI_DOUBLE, worker, 1, MPI_COMM_WORLD); \
-    } \
-}
-
-void calc(double* arr, uint32_t ySize, uint32_t xSize, int rank, int size)
-{
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    if(size == 1) {
-        for(uint32_t y = 0; y < ySize; ++y) {
-            for(uint32_t x = 0; x < xSize; ++x) {
-                arr[y*xSize + x] = sin(0.00001*arr[y*xSize + x]);
-            }
+        for(int send_rank = 0; send_rank < size; ++send_rank) {
+            // We will send start_y, step, xSize and part of arr
+            uint32_t real_step = std::min(y_step, ySize - prev_range);
+            range[send_rank] = real_step * xSize;
+            displace[send_rank] = prev_range * xSize;
+            prev_range += y_step; 
         }
-        return; 
     }
- 
-    //we will
+    MPI_Scatter(range, 1, MPI_INT,
+            &step, 1, MPI_INT,
+            ROOT, MPI_COMM_WORLD);
+
+
+    double* my_copy = (double*) calloc(step, sizeof(double));
+    MPI_Scatterv(arr, range, displace, MPI_DOUBLE, 
+            my_copy, step, MPI_DOUBLE, 
+            ROOT, MPI_COMM_WORLD);    
+
+
+    for(int32_t x = 0; x < step; ++x) {
+        my_copy[x] = sin(0.00001*my_copy[x]);
+    }
+   /*  
+    if(size > 1 && rank == 1) {
+        for(int i = 0; i < step; ++i) {
+            printf("%lf \n", my_copy[i]);
+        }
+    }*/
+    /*
     if(rank == ROOT) {
-        int worker = 0;
-        int have_task = 0;
-        for(uint32_t y = 0; y < ySize; ++y) {
-            for(uint32_t x = 0; x < xSize; ++x) {
-                MPI_recv_start_data(&worker, &have_task);  
-                
-                if(have_task != 0) { // Recv task
-                    MPI_recv_res(worker, arr, xSize);
-                }
-                
-                MPI_send_data(worker, &x, &y, arr, xSize, 1);
-            }
-        }
-        for(int i = 1; i < size; ++i) {
-            MPI_recv_start_data(&worker, &have_task);  
-            
-            if(have_task != 0) { // Recv task
-                MPI_recv_res(worker, arr, xSize);
-            }
-            
-            uint32_t x = uint32_t(-1), y = x; 
-            MPI_send_data(worker, &x, &y, arr, xSize, 0);
-        }
-    } else {
-        int have_task = 0;
-        while(1) {
-            MPI_send_start_data(&rank, &have_task);
-            have_task = 1;
-             
-            uint32_t x = 0, y = 0;
-            double arr = 0;
-            MPI_recv_data(&x, &y, &arr);
-            
-            arr = sin(0.00001*arr);
-            MPI_send_res(&x, &y, &arr);
-        }
+    for(int i = 0; i < size; ++i) {
+    printf("rang[i] = %d, dit[i] = %d\n", range[i], displace[i]);
     }
+    }*/
+
+    //printf("[%d] xSize = %u, step = %u, my_copy = %p\n", rank, xSize, step, my_copy);
+    
+    MPI_Gatherv(my_copy, step, MPI_DOUBLE, 
+            arr, range, displace, MPI_DOUBLE,
+            ROOT, MPI_COMM_WORLD);
+
+    if(rank == ROOT) {
+
+        free(range);
+        free(displace);
+    }
+    free(my_copy); 
 }
 
 int main(int argc, char** argv)
