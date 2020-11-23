f3ff13eb21d55cabe26789cb0911d543bf596775
Filipp Mikoian <philippe.mikoyan@frtk.ru>
2020-11-23 09:16:29 -0500

    Implement 'compare_curves' function via bc
    This is simple and parallelizable implementation of compare_curves. Need
    for parallelization arised from the fact that output file alone takes 70
    MB, and it takes quite some time to be processed by bc.
    
    To change number of threads, play with $procnum variable.

--- CompMath1/run.sh
+++ CompMath1/run.sh
@@ -6,7 +6,105 @@ src="./src/main.cpp"
 build="./build"
 exe="$build/task"
 tests_dir="./tests"
+delta=0.000001
+procnum=4
 mpiexecflags=
+
+function compare_curves {
+  filein1=$1
+  filein2=$2
+
+  if ! command -v bc &> /dev/null; then
+    echo "bc is required, run 'sudo apt --yes install bc'," \
+         "'sudo yum -y install bc' or something similar"
+    exit 1
+  fi
+
+  if [ ! -f "$filein1" ]; then
+    echo "File '$filein1' does not exist"
+    return 1
+  fi
+  if [ ! -f "$filein2" ]; then
+    echo "File '$filein2' does not exist"
+    return 1
+  fi
+
+  # Count and compare number of lines in input files
+  # wc also prints file num, so cut only prints linenum
+  linenum1=$(wc -l $filein1 | cut -d " " -f1)
+  linenum2=$(wc -l $filein2 | cut -d " " -f1)
+  if [ $linenum1 -ne $linenum2 ]; then
+    echo "Different number of points($linenum1 vs. $linenum2)"
+    echo "$filein1, $filein2"
+    return 1
+  fi
+  if [ $linenum1 -eq 0 ]; then
+    echo "Zero points found in both files"
+    return 1
+  fi
+
+  # Create file that will have two values in each line to compare
+  filedata=$(mktemp)
+  paste $filein1 $filein2 > $filedata
+
+  # Create file with actual bc code, which
+  # itself is pretty much obvious
+  filebccode=$(mktemp)
+  cat  >$filebccode<<EOF
+    define abs(i) {
+      if (i < 0) return (-i);
+      return (i);
+    }
+
+    off = read();
+    len = read();
+    for (p = off; p < off+len; p++) {
+      v1 = read();
+      v2 = read();
+      if (abs(v1-v2) >= $delta) {
+        print p, "\n";
+        halt;
+      }
+    }
+    print -1, "\n";
+EOF
+
+  # dry run of mktemp, get the name to create fifo
+  tmppipe=$(mktemp -u)
+  mkfifo $tmppipe
+  # Attach pipe to 3rd fd
+  exec 3<>$tmppipe
+
+  # This will divide filedata in smaller files with prefix "$filedata."
+  split -n l/$procnum $filedata "$filedata."
+
+  TIME_START=$(date +%s)
+  # Split created $procnum small files, launch bc for each of them
+  chunkoffset=0
+  for file in $(ls $filedata\.*); do
+    chunksize=$(wc -l $file | cut -d " " -f1)
+    echo $chunkoffset $chunksize $(cat $file) | bc $filebccode >&3 &
+    chunkoffset=$(($chunkoffset+$chunksize))
+  done
+
+  # Collect execution results
+  failed=0
+  for ((i=0; i < $procnum; i++)); do
+    read pointnum <&3
+    if [ $pointnum -ne -1 ]; then
+      echo "Failed point is $pointnum(numeration starts from 0)"
+      failed=1
+    fi
+  done
+  echo "bc took $(($(date +%s)-$TIME_START)) sec on $procnum CPUs"
+
+  # cleanup, remove temporary files
+  rm -f $filebccode $tmppipe $filedata $filedata.*
+  # close pipe
+  exec 3>&-
+
+  return $failed
+}
 
 # Create 01 test output file if it does not exist
 testoutfilechecksum="8dbe67f912e0ccbc53411adfeaa42b9b"
@@ -63,7 +161,8 @@ for test_dir in $tests_dir/*; do
       echo "[TEST $test] RUNTIME FAIL"
       continue;
     fi
-    if cmp -s $build/$test.txt $test_dir/output.txt; then
+    compare_curves $build/$test.txt $test_dir/output.txt
+    if (($? == 0)); then
       echo "[TEST $test] OK ($DIFF ms)"
       SUCCESS_TESTS+=($test)
     else
