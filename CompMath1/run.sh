897cc1d3029f361967afe69563b5a8da85862913
Filipp Mikoian <philippe.mikoyan@frtk.ru>
2020-11-23 19:57:56 -0500

    Allow to generate heavyweight tests/01/output.txt offline

--- CompMath1/run.sh
+++ CompMath1/run.sh
@@ -6,6 +6,31 @@ src="./src/main.cpp"
 build="./build"
 exe="$build/task"
 tests_dir="./tests"
+mpiexecflags=
+
+# Create 01 test output file if it does not exist
+testoutfilechecksum="8dbe67f912e0ccbc53411adfeaa42b9b"
+testoutfilename="tests/01/output.txt"
+testinfilename="tests/01/input.txt"
+if [ ! -f $testoutfilename ]; then
+  echo "Creating $testoutfilename file"
+  mkdir $build 2> /dev/null
+  $compiler $src -o $exe -DREFERENCE $flags
+  if [ ! $? -eq 0 ]; then
+    echo "[ERROR] Failed to compile $src with -DREFERENCE"
+    exit 1
+  fi
+  mpiexec $mpiexecflags -np 1 $exe $testinfilename $testoutfilename
+fi
+
+# Assuming that md5sum is available out of the box
+curchecksum=$(md5sum $testoutfilename | cut -d " " -f1)
+if [ $curchecksum != $testoutfilechecksum ]; then
+  echo "[WARNING] Weird, $testoutfilename checksum does not match." \
+       "Did you edit test output/code inside of REFERENCE conditional" \
+       "compilation block and then created test output? If you want" \
+       "to fix it, try deleting $testoutfilename and running run.sh again"
+fi
 
 echo "[CLEAR]"
 echo "  rm $build -r"
@@ -31,7 +56,7 @@ for test_dir in $tests_dir/*; do
     printf "\n[TEST $test]\n"
     echo "mpiexec -np $proc $exe $test_dir/input.txt $build/$test.txt"
     START=$(date +%s%N)
-    mpiexec -np $proc $exe $test_dir/input.txt $build/$test.txt
+    mpiexec $mpiexecflags -np $proc $exe $test_dir/input.txt $build/$test.txt
     END=$(date +%s%N)
     DIFF=$((($END - $START)/1000000))
     if [ ! $? -eq 0 ]; then
